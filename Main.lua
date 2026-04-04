153606e6231f818ab8b1d66dc605dec3accb4db5
makarmatvij7-svg <makarmatvij7@gmail.com>
2026-04-04 02:19:55 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -79,7 +79,7 @@ local camera = workspace.CurrentCamera
 -- ══════════════════════════════════════════
 
 local VALID_KEY = "CyberDragonOnTop"
-local KEY_URL   = "https://link-target.net/3311972/9bTwBCLEq41P"
+local KEY_URL   = "https://linkvertise.com/3311972/9bTwBCLEq41P?o=sharing"
 
 local KC = {
     bg     = Color3.fromRGB(8,   8,   12),
